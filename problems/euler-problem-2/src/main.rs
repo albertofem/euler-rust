60085520969fb5418869ca3dcdccc732cc45e689
Alberto Fernández <albertofem@gmail.com>
2015-03-01 10:18:05 +0100

    Correct solution on problem 2

--- problems/euler-problem-2/src/main.rs
+++ problems/euler-problem-2/src/main.rs
@@ -4,7 +4,7 @@ fn main() {
 	let mut addition: i32 = 0;
 
 	for &number in fibonacci.iter() {
-		if (number % 2 != 0) {
+		if number % 2 != 0 {
 			continue;
 		}
 
@@ -17,14 +17,29 @@ fn main() {
 fn calculate_fibonacci() -> Vec<i32> {
 	let mut fibonacci: Vec<i32> = Vec::new();
 
-	for x in 1..4000000 {
-		if (x == 1) {
-			fibonacci.push(x);
-		} else {
-			let position: i32 = x-2;
-			let last_term: i32 = fibonacci[position as usize];
-			fibonacci.push(x + last_term);
+	// first 2 terms
+	fibonacci.push(0);
+	fibonacci.push(1);
+
+	// start in term 3
+	let mut x: i32 = 2;
+
+	loop {
+		let position: i32 = x-1;
+		let position2: i32 = position-1;
+
+		let prev_term: i32 = fibonacci[position as usize];
+		let prev_term2: i32 = fibonacci[position2 as usize];
+
+		let fibonacci_term: i32 = prev_term + prev_term2;
+
+		fibonacci.push(fibonacci_term);
+
+		if fibonacci_term >= 4000000 {
+			break;
 		}
+
+		x += 1;
 	}
 
 	return fibonacci;
